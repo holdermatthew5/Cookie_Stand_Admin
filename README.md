063bd01c99fd481f169a70a70a1a99cfe438e7e3
Matthew Holder <holdermatthew5@gmail.com>
2021-04-15 07:06:49 -0500

    totals working

--- README.md
+++ README.md
@@ -14,6 +14,41 @@
 - [x] Have a `<footer>` component that matches spec.
 - [x] Style app using TailwindCSS utility classes.
 
+## React II
+
+- [x] `pages/Index.js` should return top level component `<CookieStandAdmin>`
+- [x] `<CookieStandAdmin>` details…
+    - [x] Have a `<Head>` component.
+    - [x] Have a custom `<Header>` component that matches spec.
+    - [x] Have a `<main>` component.
+        - [x] Within `<main>` have custom `<CreateForm>` and `<ReportTable>` components.
+    - [x] Have a custom `<Footer>` component that matches spec.
+    - [x] Import time slot data from supplied data.js file.
+- [x] `<Head>` should set page title Cookie Stand Admin
+- [x] `<Header>` component should have Next `<Link>` to overview page.
+- [x] `<CreateForm>` component details…
+    - [x] Receive an onCreate function to be called when form is submitted.
+    - [x] onCreate should be passed argument object representing new cookie stand.
+        - [x] Object should have location property.
+        - [x] Object should have hourly_sales property with hard coded [48, 42, 30, 24, 42, 24, 36, 42, 42, 48, 36, 42, 24, 36]
+- [x] `<ReportTable>` details…
+    - [x] should receive hours on props that is an array cookie stand hours of operation.
+    - [x] should receive reports on props that is an array all cookie stand objects.
+    - [x] If reports is empty then render `<h2>No Cookie Stands Available</h2>`
+    - [x] If reports is not empty then render a table with thead,tbody and tfoot components.
+    - [x] Component should render to match spec.
+    - [x] Component is responsible for tallying totals for each cookie stand as well as all cookie stands per hourly slot.
+        ----- FIND A WAY TO ADD THAT TD TO THE END OF THE LIST. HOPEFULLY CLEANLY IN THE `.map` BUT PRIOIRITIZE SPEED
+- [x] `<Footer>` component details…
+    - [x] Should receive reports array on props.
+    - [x] Should display X Locations World Wide where X is number of cookie stands.
+- [] `<Header>`,`<Footer>`,`<CreateForm>` and `<ReportTable>` should each be in own file inside top level components folder.
+- [] Those components should be imported into Index.js
+- [] Overview page details…
+- [] should live in pages/overview.js
+- [] should render `<Link href="/"><a>Return to Main page</a></Link>`
+- [] Style all components using TailwindCSS utility classes to match spec.
+
 
 <!-- # Next.js + Tailwind CSS Example
 
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
-import Head from 'next/head'
+import Link from 'next/link'
+import { hours } from '../data'
 import { useState } from 'react'
 
 export default function Home() {
 
-  const [store, setStore] = useState('')
+  const [store, setStore] = useState('');
+  const [reports, setReports] = useState([]);
 
-  function createHandler(event){
+  function onCreate(event){
     event.preventDefault();
     let store = {
       location: event.target.location.value,
@@ -14,40 +16,171 @@ export default function Home() {
       ave: event.target.ave.value,
     }
     setStore(store)
+    let storeReport = {
+      location: event.target.location.value,
+      hourlySales: [48, 42, 30, 24, 42, 24, 36, 42, 42, 48, 36, 42, 24, 36]
+    }
+    setReports([...reports, storeReport])
   }
 
   return (
-    <div className="">
-      <Head>
+    <CreateStandAdmin />
+  )
+  
+  function CreateStandAdmin(props){
+    return (
+      <div className="">
+        <Head />
+
+        <Header />
+
+        <Main />
+      </div>
+    )
+  }
+
+  function Head(props){
+    return (
+      <head>
         <title>Cookie Stand Admin</title>
         <link rel="icon" href="/favicon.ico" />
-      </Head>
+      </head>
+    )
+  }
 
-      <header className="bg-green-500 px-10 py-5 text-4xl font-bold">
-        <h1>Cookie Stand Admin</h1>
+  function Header(props){
+    return (
+      <header className="bg-green-500 px-10 py-5">
+        <h1 className="inline-block text-4xl font-bold">Cookie Stand Admin</h1>
+        <Link href="/overview" className="">
+          <a>Overview</a>
+        </Link>
       </header>
+    )
+  }
 
-      <main className="bg-green-200 w-full p-10 text-center grid justify-items-stretch px-0">
-        <form onSubmit={createHandler} className="bg-green-300 px-10 py-5 w-4/5justify-self-center rounded mx-12">
-          <h2 className="p-5 text-2xl">Create Cookie Stand</h2>
-          <label className="w-5/6">Location:<input className="w-5/6 m-3 " name="location"/></label>
-          <section className="py-5">
-            <label className="inline-block m-3">Minimum Customuers per Hour<br/><input className="w-60" name="min"/></label>
-            <label className="inline-block m-3">Maximum Customuers per Hour<br/><input className="w-60" name="max"/></label>
-            <label className="inline-block m-3">Average Cookies per Sale<br/><input className="w-60" name="ave"/></label>
-            <button className="bg-green-400 m-3 p-8 py-5" type="submit">Create</button>
-          </section>
-        </form>
-        <section className="text=center p-10">
-          <p className="m-3">Report Table Coming Soon..</p>
-          <p className="m-3">{JSON.stringify(store)}</p>
-        </section>
-        <footer className="bg-green-500 h-15 text-left p-5">&#169;2021</footer>
+  function Main(props){
+    return (
+      <main className="bg-green-200 min-w-min p-10 text-center grid justify-items-stretch px-0">
+        <CreateForm />
+        <ReportTable hours={ hours } reports={ reports } />
+        <Footer reports={ reports } />
       </main>
+    )
+  }
 
-      <footer className="">
-        
-      </footer>
-    </div>
-  )
-}
+  function CreateForm(props){
+    return (
+      <form onSubmit={(e) => onCreate(e)} className="bg-green-400 px-10 py-5 w-4/5 justify-self-center rounded mx-12">
+        <h2 className="p-5 text-2xl">Create Cookie Stand</h2>
+        <label className="w-5/6">Location:<input className="w-5/6 m-3 " name="location"/></label>
+        <section className="py-5">
+          <label className="inline-block p-3 m-2 bg-green-300">Minimum Customuers per Hour<br/><input className="w-60" name="min"/></label>
+          <label className="inline-block p-3 m-2 bg-green-300">Maximum Customuers per Hour<br/><input className="w-60" name="max"/></label>
+          <label className="inline-block p-3 m-2 bg-green-300">Average Cookies per Sale<br/><input className="w-60" name="ave"/></label>
+          <button className="bg-green-500 m-3 p-8 py-5" type="submit">Create</button>
+        </section>
+      </form>
+    )
+  }
+
+  function ReportTable(props){
+    if (props.reports === []) {
+      return (
+        <h2 className="m-10 text-2xl">No Cookie Stands Available</h2>
+      )
+    } else {
+      return (
+        <section className="justify-self-center bg-green-500 m-10 rounded">
+          <table className="">
+            <thead>
+              <HeadRow hours={ props.hours } />
+            </thead>
+            <tbody>
+              <BodyRow reports={ props.reports } />
+            </tbody>
+          </table>
+        </section>
+      )
+    }
+  }
+
+  function HeadRow(props){
+    const head = props.hours.map((hour) => 
+      <td className="inline-block px-2 font-bold">{hour}</td>
+    );
+    return (
+      <tr>
+        <th className="px-2">Location</th>
+        { head }
+      </tr>
+    )
+  }
+  
+  function BodyRow(props){
+    let temp = [];
+    if (props.reports.length > 0){
+      let totals = [];
+      let total = 0;
+      for (let i = 0; i < 14; i++){
+        total = 0;
+        for (let j = 0; j < props.reports.length; j++){
+          total += props.reports[j].hourlySales[i];
+        }
+        totals.push(total);
+      }
+      for (let i = 0; i <= props.reports.length; i++){
+        if (i < props.reports.length){
+          temp.push(props.reports[i])
+        } else {
+          temp.push({
+            "location": "Totals",
+            "hourlySales": totals,
+          })
+        }
+      }
+    }
+    const reports = temp.map(report => {
+      return (
+        <tr>
+          <th className="bg-green-300 border border-black">{ report.location }</th>
+          <TableData reports={ props.reports } hourlySales={ report.hourlySales } />
+        </tr>
+      )
+    })
+    return (
+      reports
+      )
+    }
+    
+    function TableData(props){
+      let storeTotal = 0
+      let temp = []
+      for (let i = 0; i <= props.hourlySales.length; i++){
+        if (props.hourlySales[i] !== undefined){
+          storeTotal += props.hourlySales[i];
+        }
+        if (i < props.hourlySales.length){
+          temp.push(props.hourlySales[i])
+        } else {
+          temp.push(storeTotal)
+        }
+      }
+      const sales = temp.map(sale => {
+        return (<td className="inline-block px-4 bg-green-300 border border-black">{ sale }</td>)
+      })
+    return (
+      sales
+    )
+  }
+
+  function Footer(props){
+    let count = 0
+    if (props.reports.length !== 0){
+      count = props.reports.length
+    }
+    return (
+      <footer className="bg-green-500 h-15 text-left p-5">{ count } Locations World Wide</footer>
+      )
+    }
+  }
